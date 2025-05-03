70dc044beaad4bc001377c76306ab80db7c721bb
AurelBichop <linux.aurelien@gmail.com>
2025-05-03 22:41:23 +0200

    First component (navbar)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './features/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [NavbarComponent],
+  template: '<app-navbar />',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
